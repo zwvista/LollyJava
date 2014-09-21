c11dfa7381f9ca896fdd922b06aca267ba5e954c
zwvista <zwvista@msn.com>
2014-09-21 23:56:42 +0900

    Added spring_context.xml

---
+++ src/main/java/com/zwstudio/lolly/dao/LanguageDao.java
@@ -0,0 +1,9 @@
+package com.zwstudio.lolly.dao;
+
+import java.util.List;
+
+import com.zwstudio.lolly.domain.Language;
+
+public interface LanguageDao {
+	List<Language> list();
+}
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,21 +1,20 @@
 import java.util.List;
 
-import org.hibernate.Session;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.zwstudio.lolly.dao.LanguageDao;
 import com.zwstudio.lolly.domain.Language;
-import com.zwstudio.lolly.util.HibernateUtil;
 
 
 public class Test {
 
 	public static void main(String[] args) {
-		Session ss = HibernateUtil.getSessionFactory().openSession();
-		@SuppressWarnings("unchecked")
-		List<Language> result = ss.createCriteria(Language.class).list();
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring_context.xml");
+		LanguageDao langdao = context.getBean(LanguageDao.class);
+		List<Language> result = langdao.list();
 		for(Language lang : result) {
 			System.out.println(lang.getName());
 		}
-		ss.close();
 	}
 
 }
