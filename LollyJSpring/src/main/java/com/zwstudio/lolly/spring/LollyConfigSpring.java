9807b953d28aae4b6a1cded43a70d4413b9bc114
zwvista <zwvista@msn.com>
2014-10-14 00:57:39 +0900

    Added LollyJSpring

---
+++ LollyJSpring/src/main/java/com/zwstudio/lolly/spring/LollyConfigSpring.java
@@ -0,0 +1,22 @@
+package com.zwstudio.lolly.spring;  
+  
+import org.springframework.context.annotation.Bean;  
+import org.springframework.context.annotation.ComponentScan;  
+import org.springframework.context.annotation.Configuration;  
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;  
+import org.springframework.web.servlet.view.JstlView;  
+import org.springframework.web.servlet.view.UrlBasedViewResolver;  
+  
+@Configuration
+@ComponentScan("com.zwstudio.lolly.spring")
+@EnableWebMvc   
+public class LollyConfigSpring { 
+    @Bean
+    public UrlBasedViewResolver setupViewResolver() {  
+        UrlBasedViewResolver resolver = new UrlBasedViewResolver();  
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);  
+        return resolver;
+    }
+}  
---
+++ LollyJSpring/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Lolly</h2>
+</body>
+</html>
