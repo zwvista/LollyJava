78271572362e4d5399d10d38faea9d0b5e19772d
趙偉 <zwvista@msn.com>
2016-01-08 22:08:35 +0900

    struts2 annotations

--- LollyJStruts2/src/main/java/com/zwstudio/lolly/web/struts2/LollyAction.java
+++ LollyJStruts2/src/main/java/com/zwstudio/lolly/web/struts2/LollyAction.java
@@ -2,9 +2,13 @@ package com.zwstudio.lolly.web.struts2;
  
 import java.util.List;
 
-import lombok.Getter;
-import lombok.Setter;
-
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Actions;
+import org.apache.struts2.convention.annotation.InterceptorRef;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.ParentPackage;
+import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.convention.annotation.ResultPath;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
@@ -14,7 +18,14 @@ import com.zwstudio.lolly.hibernate.dao.DictAllDao;
 import com.zwstudio.lolly.hibernate.dao.DictionaryDao;
 import com.zwstudio.lolly.hibernate.dao.LanguageDao;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Controller
+@Namespace("/")
+@ResultPath("/")
+@ParentPackage("default")
+@InterceptorRef("defaultStack")
 public class LollyAction extends ActionSupport {
  
     private static final long serialVersionUID = 1L;
@@ -39,6 +50,28 @@ public class LollyAction extends ActionSupport {
 	@Getter @Setter
 	public String url;
 
+	@Actions({
+		@Action(
+			value="lolly1",
+			results=@Result(location="lolly1.jsp")
+		),
+		@Action(
+			value="lolly12",
+			results=@Result(location="lolly12.jsp")
+		),
+		@Action(
+			value="lolly13",
+			results=@Result(location="lolly13.jsp")
+		),
+		@Action(
+			value="lolly2",
+			results=@Result(location="lolly2.jsp", type="velocity")
+		),
+		@Action(
+			value="lolly3",
+			results=@Result(location="lolly3.jsp", type="freemarker")
+		),
+	})
 	public String execute() {
 		langList = langDao.getData();
 		word = "一人";
